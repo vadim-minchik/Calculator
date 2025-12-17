5beb72269eb864cf7f56dd12f23b1467a33d5307
vadim-minchik <vadim.minchik2005@gmail.com>
2025-12-17 16:48:56 +0300

    added the CalculatorProcessor class

---
+++ src/CalculatorProcessor.java
@@ -0,0 +1,20 @@
+
+public class CalculatorProcessor {
+	
+	public static int Sum( int number1, int number2) {
+	    return number1 + number2; 
+	}
+	
+	public static int Minus(int number1, int number2) {
+	    return number1 - number2; 
+	    }
+	
+	public static int multiply(int number1, int number2) {
+	    return number1 * number2; 
+	    }
+	
+	public static int Divide(int number1, int number2) {
+	    return number1 / number2; 
+	    }
+	
+}
